12c86d1e1fa285e90bcef28e12702731c135c885
Liz Lawrence <liz@winnielabs.com>
2018-06-25 19:06:39 -0700

    update

--- scripts/script.js
+++ scripts/script.js
@@ -39,7 +39,7 @@ EXAMPLES OF THE KEY HUBOT FUNCTIONS
 
 /* Variables for random example */
 
-const squirrels = ["http://img.skitch.com/20100714-d6q52xajfh4cimxr3888yb77ru.jpg", "https://img.skitch.com/20111026-r2wsngtu4jftwxmsytdke6arwd.png", "http://cl.ly/1i0s1r3t2s2G3P1N3t3M/Screen_Shot_2011-10-27_at_9.36.45_AM.png", "http://shipitsquirrel.github.com/images/squirrel.png"];
+// const squirrels = ["http://img.skitch.com/20100714-d6q52xajfh4cimxr3888yb77ru.jpg", "https://img.skitch.com/20111026-r2wsngtu4jftwxmsytdke6arwd.png", "http://cl.ly/1i0s1r3t2s2G3P1N3t3M/Screen_Shot_2011-10-27_at_9.36.45_AM.png", "http://shipitsquirrel.github.com/images/squirrel.png"];
 
 // module.exports = function(robot) {
 //   /* Basic example of respond / send. If the user enters hi or hello the bot responds "Howdy!" */
@@ -49,6 +49,6 @@ const squirrels = ["http://img.skitch.com/20100714-d6q52xajfh4cimxr3888yb77ru.jp
 
   /* Random Example
   If a user enters 'ship it' we return a random squirrel, which is popular for symbolizing shipping something with engineers */
-  return robot.hear(/ship it/i, function(msg) {
-    return msg.send(msg.random(squirrels));
-  }); 
+  // return robot.hear(/ship it/i, function(msg) {
+  //   return msg.send(msg.random(squirrels));
+  // }); 
