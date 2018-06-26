9f3a6ecb6aa10657acc47f16c874549a0ed2bb8a
Liz Lawrence <liz@winnielabs.com>
2018-06-25 19:02:04 -0700

    update

--- scripts/script.js
+++ scripts/script.js
@@ -26,6 +26,9 @@ module.exports = function(robot) {
   robot.hear(/beautiful/i, function(res) { 
     return res.emote("did you know that according to the movie donnie darko... cellar door is the most beautiful combination of words in the english language?");
   });
+  robot.hear(/lonely/i, function(res){
+    return res.emote("")
+  });
 }
 
 /************************************
@@ -36,7 +39,7 @@ EXAMPLES OF THE KEY HUBOT FUNCTIONS
 
 /* Variables for random example */
 
-// const squirrels = ["http://img.skitch.com/20100714-d6q52xajfh4cimxr3888yb77ru.jpg", "https://img.skitch.com/20111026-r2wsngtu4jftwxmsytdke6arwd.png", "http://cl.ly/1i0s1r3t2s2G3P1N3t3M/Screen_Shot_2011-10-27_at_9.36.45_AM.png", "http://shipitsquirrel.github.com/images/squirrel.png"];
+const squirrels = ["http://img.skitch.com/20100714-d6q52xajfh4cimxr3888yb77ru.jpg", "https://img.skitch.com/20111026-r2wsngtu4jftwxmsytdke6arwd.png", "http://cl.ly/1i0s1r3t2s2G3P1N3t3M/Screen_Shot_2011-10-27_at_9.36.45_AM.png", "http://shipitsquirrel.github.com/images/squirrel.png"];
 
 // module.exports = function(robot) {
 //   /* Basic example of respond / send. If the user enters hi or hello the bot responds "Howdy!" */
@@ -44,9 +47,9 @@ EXAMPLES OF THE KEY HUBOT FUNCTIONS
 //     return msg.send("Howdy!");
 //   });
 
-//   /* Random Example
-//   If a user enters 'ship it' we return a random squirrel, which is popular for symbolizing shipping something with engineers */
-//   return robot.hear(/ship it/i, function(msg) {
-//     return msg.send(msg.random(squirrels));
-//   });
-// };
+  /* Random Example
+  If a user enters 'ship it' we return a random squirrel, which is popular for symbolizing shipping something with engineers */
+  return robot.hear(/ship it/i, function(msg) {
+    return msg.send(msg.random(squirrels));
+  });
+};
